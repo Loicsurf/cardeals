01ab7464273d364f4bd48a11af2096dba5746065
loicsurf <loicsurf20@gmail.com>
2022-03-01 17:42:33 +0100

    changes

--- cardealer/cardeal/settings.py
+++ cardealer/cardeal/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%z6g(*3eq9z+8dg(i-qpg#pw@k*(^^p#s%r5(7h9m5e1cj38w5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['fairlycardeals.herokuapp.com']
 
